5ced2668fecb234368b1cba252a94e07def1dfdc
Jason Grim <jason.grim@klarna.com>
2017-04-27 11:12:47 -0400

    Added license and other misc cleanup

--- src/Session.php
+++ src/Session.php
@@ -1,11 +1,33 @@
 <?php namespace KlarnaKco;
 
 /**
+ * Copyright 2017 Jason Grim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package    KlarnaKco
+ * @author     Jason Grim <me@jasongrim.com>
+ */
+
+use KlarnaCore\Resource;
+use KlarnaCore\Transaction;
+
+/**
  * Klarna Checkout Session
  *
  * @package KlarnaKco
  */
-class Session extends \KlarnaCore\Resource
+class Session extends Resource
 {
     /**
      * Get session details
@@ -32,7 +54,7 @@ class Session extends \KlarnaCore\Resource
      */
     public function create($data)
     {
-        if ($data instanceof \KlarnaCore\Transaction) {
+        if ($data instanceof Transaction) {
             $data = $data->toArray();
         }
 
@@ -57,7 +79,7 @@ class Session extends \KlarnaCore\Resource
      */
     public function update($id, $data)
     {
-        if ($data instanceof \KlarnaCore\Transaction) {
+        if ($data instanceof Transaction) {
             $data = $data->toArray();
         }
 
